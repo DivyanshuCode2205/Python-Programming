be97325410e96d4a8fc2848811e1dfcc65e8db30
DivyanshuCode2205 <divyanshuxd3@gmail.com>
2026-05-21 23:04:30 +0530

    Remove old Chapter 4 folder files after rename

--- Chapter 4/1_list.py
+++
@@ -1,9 +0,0 @@
-# lists are mutable data types 
-
-list = ["Apple", "Banana", "Grapes", 69.69, 69, 106, "Miles", "Shelby"]
-
-list[6] = "Kane Miles" # any change wil affect the original list
-
-print(f"New list {list}")
-
-print(f"Slicing of list: {list[1:7]}") # while indexing the end value will be one less
--- Chapter 4/3_tuples.py
+++
@@ -1,8 +0,0 @@
-# unlike lists, tuples are immutable datatypes
-
-a = () # empty tuple and type(a) retuns class <'tuple'>
-
-# but if we do something like this b = (1) then type(b) would return class <'int'>
-
-c = (1, 3, 5, 6, "Harry", "Jhoomka", 334, 706.4)
-print(type(c))
